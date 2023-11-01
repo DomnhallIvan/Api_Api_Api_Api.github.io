57c4a27e43c1b2272798df5163fe0fa052614660
Ivan PM <cdmx2125@amerike.edu.mx>
2023-10-31 23:08:34 -0600

    Ahora si

--- cosas.js
+++ cosas.js
@@ -1,7 +1,4 @@
-
-//import { amiiboSearch } from "./modulos/api-amiibo";
 import { PokeCartasSearch } from "./modulos/api-PokeCartas.js";
 
-//amiiboSearch("#search",".amiibo")
 PokeCartasSearch("#search",".cartas")
 
